0cbcc21b38c0f950c8e7128b312c2d0c72aadb88
michelleRahman21 <michgomez1807@hotmail.com>
2020-04-02 16:52:15 -0400

    Add starter front end containers

--- api/index.js
+++ api/index.js
@@ -10,23 +10,26 @@ const headers = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET'
 };
-
+// Helper functions
 const decodedParams = searchParams => Array
     .from(searchParams.keys())
     .reduce((acc, key) => ({...acc, [key]: searchParams.get(key)}), {});
-
-const  server = createServer((req, res) => {
+// Create the functions for the server to request and respond to those requests.
+  const  server = createServer((req, res) => {
   const requestURL = url.parse(req.url);
   const decodedParams = decodedParams(new URLSearchParams(requestURL.search));
-  const { search, location, country = 'gb'} = decodedParams;
+  // Change the country param accordingly to the location you want to look for, example; London = 'gb'
+  const { search, location, country = 'us'} = decodedParams;
 
   const targetURL = `${config.BASE_URL}/${country.toLowerCase()}/${config.BASE_PARAMS}&app_id=${config.APP_ID}&app_key=${config.API_KEY}&what=${search}&where=${location}`;
-
+// Sending that information to Axios
   if (req.method === 'GET') {
     console.log(chalk.green('Proxy GET request to: ${targetURL}'));
+    // send the request to axios
     axios.get(targetURL)
         .then(response => {
           res.writeHead(200, headers);
+          // it holds the info back from Adzuna
           res.end(JSON.stringify(repsonse.data));
         })
         .catch(error => {
@@ -34,9 +37,9 @@ const  server = createServer((req, res) => {
            res.writeHead(500, headers);
            res.end(JSON.stringify(error))
         });
-  }
+    }
 });
-
-server.listen(3105, () => {
+// set the server listening
+server.listen(3000, () => {
   console.log(chalk.green('Server Listening'));
 })
