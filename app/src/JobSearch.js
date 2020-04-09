33f60994d1a0878b3f8ab301cad71f3104ac8655
michelleRahman21 <michgomez1807@hotmail.com>
2020-04-08 20:08:25 -0400

    Fetching data from the API

--- app/src/JobSearch.js
+++ app/src/JobSearch.js
@@ -8,4 +8,14 @@ class JobSearch {
     this.resultsContainer = document.querySelector(resultsContainerSelector);
     this.loadingElement = document.querySelector(loadingElementSelector);
   }
+  setCountryCode(){
+    this.countryCode = 'gb';
+    this.setCurrencyCode();
+    fetch('http://ip-api.com/json')
+    .then(results => results.json())
+    .then(results => {
+      this.setCountryCode = results.countryCode.toLowerCase();
+      this.setCurrencySymbol();
+    });
+  }
 }
